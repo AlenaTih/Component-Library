5dc370b680c053347efd777f1239f1b0b4ba5574
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-11-08 02:28:29 +0300

    Update Testimonial and Tootip

--- src/components/Testimonial/TestimonialWithoutImage.tsx
+++ src/components/Testimonial/TestimonialWithoutImage.tsx
@@ -21,8 +21,10 @@ function TestimonialWithoutImage({
                 alt={`The logo of ${company}`} />
             <p className=" testimonial-text">{text}</p>
             <div className="testimonial-name-company-title-container">
-                <p className="testimonial-name">{name}</p>
-                <p className="testimonial-company-title">{`${company}, ${title}`}</p>
+                <p className="testimonial-name">{name}
+                    <span className="testimonial-slash"> /</span>
+                </p>
+                <p className="testimonial-company-title">{`${ company}, ${title}`}</p>
             </div>
             {/* {children} */}
         </div>
